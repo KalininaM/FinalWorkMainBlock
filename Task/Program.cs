f8a16294f2935143ac36dda98ecc4582ee5ec63d
Marina <shalneva_mg@mail.ru>
2023-05-03 15:47:19 +0400

    Added program task

---
+++ Task/Program.cs
@@ -0,0 +1,30 @@
+﻿﻿string[] arrayStart = {"1234", "1567", "-2", "computer science", "код"};
+string[] arrayFinal = new string[arrayStart.Length];
+
+FinalArrayStrings(arrayStart, arrayFinal);
+Console.WriteLine("Начальный массив: ");
+PrintArray(arrayStart);
+Console.WriteLine("Финальный массив: ");
+PrintArray(arrayFinal);
+
+void FinalArrayStrings(string[] arrayStart, string[] arrayFinal)
+{
+    int j = 0;
+    for (int i = 0; i < arrayStart.Length; i++)
+    {
+        if(arrayStart[i].Length <= 3)
+        {
+            arrayFinal[j] = arrayStart[i];
+            j++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
